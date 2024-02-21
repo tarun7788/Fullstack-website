63af6ebdbca75a153969f90cf2feef6a904d89ea
tarun7788 <tarunsaraswat77@gmail.com>
2024-02-22 02:38:08 +0530

    commit

--- rentit/src/Components/home.js
+++ rentit/src/Components/home.js
@@ -1,10 +1,14 @@
 import '../CSS/home.css';
 
-function Home(){
+
+const Home =()=>{
 return(
     <div className='image'>
     <img src={require('../images/logo2.png')}/>
-    <div className='home'><h1>A outfit for one time use, Naahh!!</h1><h2>JUST-RENT it...</h2><h5>Let's Normalize clothing renting..</h5></div>
+    <div className='home'>
+        <h1><span>A outfit for one time use</span>, Naahh!! <br/> JUST-RENT it...</h1>
+        <h5>Let's Normalize clothing renting..</h5>
+        </div>
     <button>Shop Now</button>
     </div>
 )
